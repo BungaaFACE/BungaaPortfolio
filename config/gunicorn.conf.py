6d8e605de9a6224b141fc973f4e506d9ebfe803d
BungaaFACE <vladislav0218@gmail.com>
2024-11-06 20:02:28 +0300

    feature: added access and error logs for gunicorn

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,3 +1,6 @@
 bind = f'0.0.0.0:8000'
 workers = 8
-loglevel = 'INFO'
+loglevel = 'DEBUG'
+accesslog = 'access.log'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+errorlog = 'error.log'
