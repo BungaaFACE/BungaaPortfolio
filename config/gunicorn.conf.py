df922511eba3f7180e2f9130ffeddbd5fd8e3ef4
BungaaFACE <vladislav0218@gmail.com>
2024-11-06 20:05:24 +0300

    change: path of logs fix: removed echo

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,6 +1,6 @@
 bind = f'0.0.0.0:8000'
 workers = 8
 loglevel = 'DEBUG'
-accesslog = 'access.log'
+accesslog = '/data/log/access.log'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-errorlog = 'error.log'
+errorlog = '/data/log/error.log'
